77be3a78473858bbc2fee244e87b5334b63649bb
natsuking <djyamaexpert@yahoo.co.jp>
2018-05-19 06:03:03 +0900

    bring back old header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,29 +10,15 @@ class Header extends Component {
   }
 
   render(){
-
-    const fontStyle = {
-      fontFamily: 'Luckiest Guy',
-      fontFamily: 'Bungee'
-    }
-
     return(
-      <nav className="navbar navbar-expand
-                    navbar-light bg-light
-                    mb-3 p-3">
-        <h2 className="mb-0" >
-          <span className="bg-dark text-white p-2 mr-2">猫</span>
-          <span className="d-none d-md-inline"
-            style={fontStyle}
-
-            >NekoTube</span>
-        </h2>
-        <form className="form-group my-2 my-lg-0 ml-sm-2 ">
-          <input
-            onChange={this.onInputChangeHandler} value={this.state.keyword}
-            className="form-control form-control-lg mr-sm-2" type="text" placeholder="検索..." aria-label="検索..." />
-        </form>
-      </nav>
+      <div style={{textAlign: 'center'}}>
+        <div>
+          <h2>NekoTube</h2>
+        </div>
+        <div>
+          <input onChange={this.onInputChangeHandler} value={this.state.keyword} />
+        </div>
+      </div>
     );
   }
 }
